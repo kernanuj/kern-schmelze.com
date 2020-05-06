f7a68c43074df46b316dd577bd75d4c9a0b18cd9
inventivo <harder@inventivo.de>
2020-05-06 17:00:59 +0200

    Custom Theme created and installed.

--- custom/plugins/InvKernschmelzeTemplate/src/InvKernschmelzeTemplate.php
+++ custom/plugins/InvKernschmelzeTemplate/src/InvKernschmelzeTemplate.php
@@ -1,11 +1,11 @@
 <?php declare(strict_types=1);
 
-namespace KernschmelzeTemplate;
+namespace InvKernschmelzeTemplate;
 
 use Shopware\Core\Framework\Plugin;
 use Shopware\Storefront\Framework\ThemeInterface;
 
-class KernschmelzeTemplate extends Plugin implements ThemeInterface
+class InvKernschmelzeTemplate extends Plugin implements ThemeInterface
 {
     public function getThemeConfigPath(): string
     {
