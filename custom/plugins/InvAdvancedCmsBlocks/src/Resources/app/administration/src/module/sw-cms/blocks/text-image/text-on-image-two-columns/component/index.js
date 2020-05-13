d5ac7f1174acd2136b49cae1c1dce1494717e7d4
inventivo <harder@inventivo.de>
2020-05-13 16:35:51 +0200

    InvTrustBar Plugin individual development. Template homepage. InvAdvancedCmsBlocks Plugin individual development.

--- custom/plugins/InvAdvancedCmsBlocks/src/Resources/app/administration/src/module/sw-cms/blocks/text-image/text-on-image-two-columns/component/index.js
+++
@@ -1,8 +0,0 @@
-import template from './sw-cms-block-text-on-image-two-columns.html.twig';
-import './sw-cms-block-text-on-image-two-columns.scss';
-
-const { Component } = Shopware;
-
-Component.register('sw-cms-block-text-on-image-two-columns', {
-    template
-});
--- custom/plugins/InvAdvancedCmsBlocks/src/Resources/app/administration/src/module/sw-cms/blocks/text-image/text-on-image-two-columns/preview/index.js
+++
@@ -1,8 +0,0 @@
-import template from './sw-cms-preview-text-on-image-two-columns.html.twig';
-import './sw-cms-preview-text-on-image-two-columns.scss';
-
-const { Component } = Shopware;
-
-Component.register('sw-cms-preview-text-on-image-two-columns', {
-    template
-});
