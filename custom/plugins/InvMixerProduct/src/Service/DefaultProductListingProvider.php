c472f21d5467f6526217a7aadf4229483fcbc3fc
sdg <sdg@posteo.de>
2020-07-11 17:04:43 +0200

    #29 Add filter by tags for mixable product items

--- custom/plugins/InvMixerProduct/src/Service/DefaultProductListingProvider.php
+++ custom/plugins/InvMixerProduct/src/Service/DefaultProductListingProvider.php
@@ -3,11 +3,13 @@
 namespace InvMixerProduct\Service;
 
 use InvMixerProduct\Constants;
+use InvMixerProduct\Repository\TagRepository;
 use InvMixerProduct\Value\ListingProductCollection;
 use InvMixerProduct\Value\ListingProductGroup;
 use Shopware\Core\Content\Product\SalesChannel\Listing\ProductListingLoader;
 use Shopware\Core\Framework\DataAbstractionLayer\Search;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
+use Shopware\Core\System\Tag\TagEntity;
 
 /**
  * Class DefaultProductListingProvider
@@ -22,14 +24,20 @@ class DefaultProductListingProvider implements ProductListingProviderInterface
     private $productListingLoader;
 
     /**
+     * @var TagRepository
+     */
+    private $tagRepository;
+
+    /**
      * DefaultProductListingProvider constructor.
      * @param ProductListingLoader $productListingLoader
+     * @param TagRepository $tagRepository
      */
-    public function __construct(ProductListingLoader $productListingLoader)
+    public function __construct(ProductListingLoader $productListingLoader, TagRepository $tagRepository)
     {
         $this->productListingLoader = $productListingLoader;
+        $this->tagRepository = $tagRepository;
     }
-
 
     /**
      * @param SalesChannelContext $salesChannelContext
@@ -40,10 +48,39 @@ class DefaultProductListingProvider implements ProductListingProviderInterface
         SalesChannelContext $salesChannelContext
     ): ListingProductCollection {
 
+        $requiredTag = $this->tagRepository->findOneByName(
+            Constants::CATALOG_PRODUCT_TAG_PRODUCT_ITEM,
+            $salesChannelContext->getContext()
+        );
+
         $collection = new ListingProductCollection();
         foreach (Constants::VALID_CATALOG_PRODUCT_TAGS() as $_tagIdentifier) {
+
+            $tags = $this->tagRepository->findAllByName(
+                $_tagIdentifier,
+                $salesChannelContext->getContext()
+            );
+
+            $tagFilters = [];
+            foreach ($tags as $tag) {
+                \assert($tag instanceof TagEntity);
+                $tagFilters[] = new Search\Filter\ContainsFilter('tagIds', $tag->getId());
+            }
+
             $productListing = $this->productListingLoader->load(
-                (new Search\Criteria()),
+                (new Search\Criteria())
+                    ->addFilter(
+                        new Search\Filter\MultiFilter(
+                            Search\Filter\MultiFilter::CONNECTION_OR,
+                            $tagFilters
+                        )
+                    )
+                    ->addFilter(
+                        new Search\Filter\ContainsFilter(
+                            'tagIds',
+                            $requiredTag->getId()
+                        )
+                    ),
                 $salesChannelContext
             );
             $collection->addGroup(
