210f420a0b624fa56ea95c64e52411cd49722212
inventivo <harder@inventivo.de>
2020-07-24 09:52:40 +0200

    Administration: Added CMS Block Four Columns Text and Image

---
+++ custom/plugins/InvAdvancedCmsBlocks/src/Resources/app/administration/src/module/sw-cms/blocks/text-image/image-text-row-four-columns/component/index.js
@@ -0,0 +1,8 @@
+import template from './sw-cms-block-image-text-row-four-columns.html.twig';
+import './sw-cms-block-image-text-row-four-columns.scss';
+
+const { Component } = Shopware;
+
+Component.register('sw-cms-block-image-text-row-four-columns', {
+    template
+});
---
+++ custom/plugins/InvAdvancedCmsBlocks/src/Resources/app/administration/src/module/sw-cms/blocks/text-image/image-text-row-four-columns/preview/index.js
@@ -0,0 +1,8 @@
+import template from './sw-cms-preview-image-text-row-four-columns.html.twig';
+import './sw-cms-preview-image-text-row-four-columns.scss';
+
+const { Component } = Shopware;
+
+Component.register('sw-cms-preview-image-text-row-four-columns', {
+    template
+});
