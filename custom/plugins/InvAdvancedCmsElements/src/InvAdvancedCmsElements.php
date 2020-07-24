564361fb9722925a06ea8e5e3445a292aed11fcf
inventivo <harder@inventivo.de>
2020-07-24 12:11:46 +0200

    Deleted unused and unfinished InvAdvancedCmsElements Plugin

--- custom/plugins/InvAdvancedCmsElements/src/InvAdvancedCmsElements.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace InvAdvancedCmsElements;
-
-use Shopware\Core\Framework\Plugin;
-
-class InvAdvancedCmsElements extends Plugin
-{
-}
--- custom/plugins/InvAdvancedCmsElements/src/Resources/app/administration/src/main.js
+++
@@ -1,7 +0,0 @@
-import './module/sw-cms/elements/two-cols-text-image';
-
-import deDE from './module/sw-cms/snippet/de-DE.json';
-import enGB from './module/sw-cms/snippet/en-GB.json';
-
-Shopware.Locale.extend('de-DE', deDE);
-Shopware.Locale.extend('en-GB', enGB);
