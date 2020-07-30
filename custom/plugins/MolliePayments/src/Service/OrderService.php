48b3c483ecc3dac5bf97743621cd2d9f5a2ff9f8
inventivo <harder@inventivo.de>
2020-07-30 16:53:46 +0200

    Plugin update: Mollie -> Please check!

--- custom/plugins/MolliePayments/src/Service/OrderService.php
+++ custom/plugins/MolliePayments/src/Service/OrderService.php
@@ -23,6 +23,8 @@ class OrderService
 {
     public const ORDER_LINE_ITEM_ID = 'orderLineItemId';
 
+    private const LINE_ITEM_TYPE_CUSTOM_PRODUCTS = 'customized-products';
+
     private const TAX_ARRAY_KEY_TAX = 'tax';
     private const TAX_ARRAY_KEY_TAX_RATE = 'taxRate';
     private const TAX_ARRAY_KEY_PRICE = 'price';
@@ -110,7 +112,7 @@ class OrderService
     {
         // Variables
         $lines = [];
-        $lineItems = $order->getLineItems();
+        $lineItems = $order->getNestedLineItems();
 
         if ($lineItems === null || $lineItems->count() === 0) {
             return $lines;
@@ -199,7 +201,7 @@ class OrderService
 
             // Build the order lines array
             $lines[] = [
-                'type' =>  $this->getLineItemType($item),
+                'type' => $this->getLineItemType($item),
                 'name' => $item->getLabel(),
                 'quantity' => $item->getQuantity(),
                 'unitPrice' => $this->getPriceArray($currencyCode, $unitPrice),
@@ -242,7 +244,6 @@ class OrderService
 
         // Get shipping tax
         $shippingTax = null;
-        $shippingTaxes = null;
 
         if ($shipping->getCalculatedTaxes() !== null) {
             $shippingTax = $this->getLineItemTax($shipping->getCalculatedTaxes());
@@ -263,7 +264,7 @@ class OrderService
 
         // Add tax when order is net
         if ($order->getTaxStatus() === CartPrice::TAX_STATE_NET) {
-            $unitPrice *= (100 + $vatRate) / 100;
+            $unitPrice *= ((100 + $vatRate) / 100);
             $totalAmount += $vatAmount;
         }
 
@@ -322,6 +323,10 @@ class OrderService
         if ($item->getType() === PromotionProcessor::LINE_ITEM_TYPE ||
             $item->getTotalPrice() < 0) {
             return OrderLineType::TYPE_DISCOUNT;
+        }
+
+        if ($item->getType() === static::LINE_ITEM_TYPE_CUSTOM_PRODUCTS) {
+            return OrderLineType::TYPE_PHYSICAL;
         }
 
         return OrderLineType::TYPE_DIGITAL;
