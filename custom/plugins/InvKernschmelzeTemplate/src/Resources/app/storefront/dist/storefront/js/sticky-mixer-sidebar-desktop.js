49f633d6bba274d1b08ba263912743128b0c50c9
inventivo <harder@inventivo.de>
2020-08-09 10:57:40 +0200

    #Sticky Auswahlfeld  Desktop (s. meine Mail vom 4.8. und wie besprochen), so dass man die gewählten Zutaten immer im Sichtfeld hat.

--- custom/plugins/InvKernschmelzeTemplate/src/Resources/app/storefront/dist/storefront/js/sticky-mixer-sidebar-desktop.js
+++ custom/plugins/InvKernschmelzeTemplate/src/Resources/app/storefront/dist/storefront/js/sticky-mixer-sidebar-desktop.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
     var mixerProductOffcanvasInner = $('#mixer-product-offcanvas .inner');
     var headerMain = $('.header-main');
     var footerMain = $('.footer-main');
+    var stickyWrapper = $('.sticky-wrapper');
 
     if (mixerProductOffcanvas.length) {
         var sidebarStickyTop = mixerProductOffcanvas.offset().top;
@@ -13,19 +14,28 @@ $(document).ready(function(){
         $(window).scroll(function (event) {
             invMakeMixerSidebarSticky(sidebarStickyTop);
         });
+
+        $(window).resize(function (event) {
+            invMakeMixerSidebarSticky(sidebarStickyTop);
+        });
     }
 
     function invMakeMixerSidebarSticky(sidebarStickyTop) {
         var scroll = $(window).scrollTop();
 
+        var parentwidth = stickyWrapper.width();
+
         if (scroll + mixerProductOffcanvasInner.height() > footerMain.offset().top) {
             mixerProductOffcanvas.removeClass("sticky");
             mixerProductOffcanvas.addClass("sticky-bottom");
+            mixerProductOffcanvasInner.width('auto');
         } else if (scroll > sidebarStickyTop) {
             mixerProductOffcanvas.addClass("sticky");
+            mixerProductOffcanvasInner.width(parentwidth-60);
             mixerProductOffcanvas.removeClass("sticky-bottom");
         } else {
             mixerProductOffcanvas.removeClass("sticky");
+            mixerProductOffcanvasInner.width('auto');
             mixerProductOffcanvas.removeClass("sticky-bottom");
         }
     }
