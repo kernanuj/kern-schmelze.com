908bfb38059dc88056dae6ea4a07c63ae85436c7
inventivo <harder@inventivo.de>
2020-10-28 12:12:38 +0100

    #KS: Video Startseite mit Lazy Load Funktion versehen

--- custom/plugins/InvKernschmelzeTemplate/src/Resources/app/storefront/dist/storefront/js/lazyload-video.js
+++ custom/plugins/InvKernschmelzeTemplate/src/Resources/app/storefront/dist/storefront/js/lazyload-video.js
@@ -1,9 +1,30 @@
 $(document).ready(function() {
-    $("video.lazy source").each(function() {
-        var sourceFile = $(this).attr("data-src");
-        $(this).attr("src", sourceFile);
-        var video = this.parentElement;
-        video.load();
-        video.play();
-    });
+    function loadLazyVideo() {
+        var el = $("video.lazy"),
+            offset = el.offset(),
+            scrollTop = $(window).scrollTop();
+
+        if ((scrollTop > offset.top - 700)) {
+            $("video.lazy source").each(function() {
+                var sourceFile = $(this).attr("data-src");
+                $(this).attr("src", sourceFile);
+                var video = this.parentElement;
+                $(this).parent('video').removeClass('lazy');
+                video.load();
+                video.play();
+            });
+        }
+    }
+
+    if($("video#video-container.lazy").length) {
+        if(!$("video.lazy source").attr("src")) {
+            loadLazyVideo();
+
+            $(window).scroll(function() {
+                if($("video.lazy").length) {
+                    loadLazyVideo();
+                }
+            });
+        }
+    }
 });
